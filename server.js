27c20c51efecba4e02ee61636831ec949a1febca
abaek <andybaek95@gmail.com>
2014-07-29 18:15:09 -0400

    changed folders again

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var cors = require('cors');
 var numUsers = 0;
+var path = require('path');
 
 io.on('connection', function(socket){
 	numUsers++;
@@ -18,19 +19,26 @@ io.on('connection', function(socket){
   });
 });
 
-app.use(cors());
-app.use('/client', express.static(__dirname + '/../client'));
-app.set('port', process.env.PORT || 3000);
-app.all('*', function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With, Accept, ContactCenterId, Content-Type');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    next();
+// app.use(cors());
+app.engine('html', require('ejs').renderFile);
+app.use('/client', express.static(__dirname + '/client'));
+app.set('views', path.join(__dirname, 'client'));
+
+app.get('/', function(req, res) {
+  res.render('schat.html');
 });
 
-app.post('/dictionary', function(req, res) {
-    res.send(req.body);
-});
+// //app.set('port', process.env.PORT || 3000);
+// app.all('*', function(req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Headers', 'X-Requested-With, Accept, ContactCenterId, Content-Type');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+//     next();
+// });
+
+// app.post('/dictionary', function(req, res) {
+//     res.send(req.body);
+// });
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
