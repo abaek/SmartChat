f54fd884a1f098d83e3cd01bcd62ac0407d7f91f
abaek <andybaek95@gmail.com>
2014-07-30 22:40:08 -0400

    server bug fix

--- server.js
+++ server.js
@@ -1,24 +1,55 @@
-var express = require('express');
-var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-var cors = require('cors');
-var path = require('path');
+var express = require('express'),
+    app = express(),
+    server = require('http').Server(app),
+    io = require('socket.io').listen(server);
+
+// Configuration
+  app.set('views', __dirname);
+  app.set('view engine', 'ejs');
+  app.use(express.static(__dirname));
+
+
+// Heroku won't actually allow us to use WebSockets
+// so we have to setup polling instead.
+// https://devcenter.heroku.com/articles/using-socket-io-with-node-js-on-heroku
+  io.set("transports", ["xhr-polling"]);
+  io.set("polling duration", 10);
+
+// Routes
+
+var port = process.env.PORT || 5000; // Use the port that Heroku provides or default to 5000
+app.listen(port, function() {
+  console.log("Express server listening on port %d in %s mode", port, app.settings.env);
+});
+
+app.get('/', 'schat.html');
+
+var status = "All is well.";
 
 var numUsers = 0;
 
-app.set('views', __dirname);
-app.engine('html', require('ejs').renderFile);
-app.use(express.static(__dirname));
+io.sockets.on('connection', function (socket) {
+  numUsers++;
+  socket.on('new user', function(username){
+    io.sockets.emit('new user', numUsers);
+  });
 
-app.get('/', function(req, res) {
-  res.render('schat.html');
+  socket.on('disconnect', function(){
+    numUsers--;
+    io.sockets.emit('new user', numUsers);
+    //console.log('disconnected');
+  });
+
+  socket.on('chat message', function(msg){
+    io.sockets.emit('chat message', msg);
+  });
 });
 
-io.set('heartbeat timeout', 5);
+
+
 
 io.on('connection', function(socket){
-  numUsers++;
+  
   //console.log('connected');
 
   socket.on('new user', function(username){
