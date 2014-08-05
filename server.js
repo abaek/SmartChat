772083ca4e1feead91e6869f416649e1f741c31f
abaek <andybaek95@gmail.com>
2014-08-05 19:47:55 -0400

    server changes

--- server.js
+++ server.js
@@ -1,58 +1,26 @@
 var express = require('express'),
     app = express(),
-    server = require('http').Server(app),
-    io = require('socket.io').listen(server);
-
-// Configuration
-  app.set('views', __dirname);
-  app.set('view engine', 'ejs');
-  app.use(express.static(__dirname));
+    path = require('path');
+    http = require('http');
+    server = http.Server(app),
+    io = require('socket.io')(server);
 
 
-// Heroku won't actually allow us to use WebSockets
-// so we have to setup polling instead.
-// https://devcenter.heroku.com/articles/using-socket-io-with-node-js-on-heroku
-  io.set("transports", ["xhr-polling"]);
-  io.set("polling duration", 10);
+  app.set('views', path.join(__dirname, ''));
+  app.set('view engine', require('ejs').renderFile);
+  app.use(express.static(path.join(__dirname, '')));
 
-// Routes
-
-var port = process.env.PORT || 5000; // Use the port that Heroku provides or default to 5000
-app.listen(port, function() {
-  console.log("Express server listening on port %d in %s mode", port, app.settings.env);
+app.get('/', function(req, res) {
+  res.render('schat.html');
 });
-
-app.get('/', 'schat.html');
-
-var status = "All is well.";
 
 var numUsers = 0;
 
-io.sockets.on('connection', function (socket) {
-  numUsers++;
-  socket.on('new user', function(username){
-    io.sockets.emit('new user', numUsers);
-  });
-
-  socket.on('disconnect', function(){
-    numUsers--;
-    io.sockets.emit('new user', numUsers);
-    //console.log('disconnected');
-  });
-
-  socket.on('chat message', function(msg){
-    io.sockets.emit('chat message', msg);
-  });
-});
-
-
-
-
 io.on('connection', function(socket){
-  
-  //console.log('connected');
+
 
   socket.on('new user', function(username){
+    numUsers++;
     io.emit('new user', numUsers);
   });
 
@@ -70,4 +38,6 @@ io.on('connection', function(socket){
 
 var port = process.env.PORT || 5000;
 
-server.listen(port, process.env.IP);
+server.listen(port, function() {
+  console.log ('Started listening on port: ' + port);
+});
